36faa6b3d4abe2d12bd149eb5edcf7af8133f877
Christopher Hogan <hogancr@Christophers-Mac-mini.local>
2016-06-21 21:03:42 -0400

    honesty policy statement as well as other files that weren't picked up on by XCode?

---
+++ assn0/assn0.playground/Contents.swift
@@ -0,0 +1,9 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+var str = "Hello, world"
+
+print(str)
+
+print("I have read, understand, and agree to the Academic Honesty Policy, including citation requirements")
